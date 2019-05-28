b1270c4e462c24e6e29aec09dc9657ee931e9774
Iavor Diatchki <iavor.diatchki@gmail.com>
2019-05-28 11:45:14 -0700

    Print more stuff, just to see.

--- crucible-mc/exe/Print.hs
+++ crucible-mc/exe/Print.hs
@@ -1,7 +1,10 @@
-{-# Language OverloadedStrings #-}
+{-# Language OverloadedStrings, TypeFamilies #-}
 module Print where
 
 import Text.PrettyPrint
+
+import Data.Parameterized.Some
+
 import Lang.Crucible.Simulator.ExecutionTree
 
 
@@ -14,14 +17,40 @@ ppExecState st =
     CallState {} -> "CallState"
     TailCallState {} -> "TailCallState"
     ReturnState {} -> "ReturnState"
-    RunningState {} -> "RunningState"
+    RunningState ri _st -> "RunningState" <+> ppRunningStateInfo ri
     SymbolicBranchState {} -> "SymbolicBranchState"
-    ControlTransferState {} -> "ControlTransferState"
+
+    ControlTransferState res _st ->
+      "ControlTransferState" <+> ppControlResumption res
+
     OverrideState {} -> "OverrideState"
     BranchMergeState t _st -> "BranchMergeState" <+> text (ppBranchTarget t)
     InitialState {} -> "InitialState"
 
+ppControlResumption :: ControlResumption p sym ext rtp f -> Doc
+ppControlResumption res =
+  case res of
+    ContinueResumption jmp -> "Continue" <+> ppResolvedJump jmp
+    CheckMergeResumption jmp -> "CheckMerge" <+> ppResolvedJump jmp
+    SwitchResumption _opts -> "Switch"
+    OverrideResumption _code _regs -> "Override"
 
+ppResolvedJump :: ResolvedJump sym blocks -> Doc
+ppResolvedJump jmp =
+  case jmp of
+    ResolvedJump bid _regs -> ppShow bid
+
+ppRunningStateInfo :: RunningStateInfo blocks args -> Doc
+ppRunningStateInfo rs =
+  case rs of
+    RunBlockStart bid      -> "BlockStart"      <+> ppShow bid
+    RunBlockEnd (Some bid) -> "BlockEnd"        <+> ppShow bid
+    RunReturnFrom f        -> "ReturnFrom"      <+> ppShow f
+    RunPostBranchMerge bid -> "PostBranchMerge" <+> ppShow bid
+
+
+ppShow :: Show a => a -> Doc
+ppShow = text . show
 
 
 
